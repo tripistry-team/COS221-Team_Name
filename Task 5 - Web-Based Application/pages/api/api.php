3491835535a84227946f98233c1b3bb99cf83116
RayhaanAhmed08 <u25047762@tuks.co.za>
2026-05-19 19:44:47 +0200

    Fixed merge conflict

--- Task 5 - Web-Based Application/pages/api/api.php
+++ Task 5 - Web-Based Application/pages/api/api.php
@@ -64,20 +64,6 @@ else if ($data["type"] === "GetDestinations") {
 else if ($data["type"] === "InsertEndpointHere") {
     echo json_encode($api->insertEndpointHere($data));
 }
-
-//Rayhaan
-else if ($data["type"] === "GetPackages") {
-    echo json_encode($api->getPackages($data));
-}
-
-else if ($data["type"] === "GetPackageDetail") {
-    echo json_encode($api->getPackageDetail($data));
-}
-
-else if ($data["type"] === "GetDestinations") {
-    echo json_encode($api->getDestinations());
-}
-
 else {
     http_response_code(400);
     echo json_encode([
